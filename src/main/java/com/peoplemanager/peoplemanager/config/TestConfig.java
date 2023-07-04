981463852222cc4d26257c19ddf6d38ef6fe4a41
VitorSiilva2 <vitoralves450@gmail.com>
2023-07-04 15:41:13 -0300

    Initial commit, Add Users

---
+++ src/main/java/com/peoplemanager/peoplemanager/config/TestConfig.java
@@ -0,0 +1,27 @@
+package com.peoplemanager.peoplemanager.config;
+
+import com.peoplemanager.peoplemanager.domain.User;
+import com.peoplemanager.peoplemanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User(null, "camila@gmail.com", "12345678", "Camila");
+        User u2 = new User(null, "vitor@gmail.com", "123456789", "Vitor");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
---
+++ src/main/java/com/peoplemanager/peoplemanager/domain/User.java
@@ -0,0 +1,86 @@
+package com.peoplemanager.peoplemanager.domain;
+
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+@Entity
+@Table(name = "TB_USER")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String email;
+    private String password;
+    private String name;
+
+    public User() {
+    }
+
+    public User(UUID id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
